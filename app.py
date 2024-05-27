22272a8eaa2a25f1fa4c91ae7527f1f197378d51
Bhake <sarbhake@amazon.com>
2024-05-27 17:24:30 +0530

    Min Max Average Feature Pushed

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ import pandas as pd
 
 
 st.title('GWC - Global Wage Calculator')
-df=pd.read_excel("v1-data.xlsx",sheet_name="Sheet3")
+df=pd.read_excel("test-1.xlsx")
 # st.write(df)
 def main():
     selected_country = st.selectbox("Select Country",df["Country"].unique())
-    selected_categories=st.multiselect("Select Category of Labour",df["Labour Type New"].unique())
+    selected_categories=st.multiselect("Select Category of Labour",df["Job Title"].unique())
+    wage_type=st.selectbox("Select the Type of Wage",["Min","Max","Average"])
     for category in selected_categories:
         num_people=st.number_input(f"Number of {category} units:",min_value=0,step=1,key=category)
         # st.write(f"{category} : {num_people} units")
@@ -20,13 +21,14 @@ def main():
         grand_total=0
         for category in selected_categories:
             num_people=st.session_state.get(category,0)
-            monthly_wage = df[(df['Country'] == selected_country) & (df['Labour Type New'] == category)]['Wage'].values[0]
-            latest_year=df[(df['Country'] == selected_country) & (df['Labour Type New'] == category)]['Year'].values[0]
+            wage_type_df=df[wage_type]
+            wage = df[(df['Country'] == selected_country) & (df['Job Title'] == category) ][wage_type].values[0]
+            # latest_year=df[(df['Country'] == selected_country) & (df['Labour Type New'] == category)]['Year'].values[0]
             # st.write(monthly_wage)
-            monthly_cost = num_people * monthly_wage 
-            grand_total += monthly_cost
+            annual_cost = num_people * wage 
+            grand_total += annual_cost
             # st.write(f"{category}: {monthly_cost}")
-            data_list.append({"Labour Type":category,"Year": latest_year,"Monthly Wage":monthly_wage,"Number of Units":num_people,"Montly Cost":monthly_cost})
+            data_list.append({"Labour Type":category,"Year":2024,"Annual Wage":round(wage,2),"Number of Units":num_people,"Annual Cost":annual_cost})
         result_df=pd.DataFrame(data_list)
         st.write(" ")
         st.write(" ")
