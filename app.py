49ac2dc4ba1473ecc571fbb51176c7f3642e61f3
Bhake <sarbhake@amazon.com>
2024-06-26 14:59:40 +0530

    All working fine as per requirements given

--- app.py
+++ app.py
@@ -11,29 +11,27 @@ def main():
     wage_type=st.selectbox("Select the Type of Wage",["Min","Max","Average"])
     st.write(" ")
     st.write(" ")
-    for category in selected_categories:
-        cols=st.columns(2)
-        with cols[0]:
-            num_people=st.number_input(f"Number of {category} personnel:",min_value=0,step=1,key=category)
-        with cols[1]:
-            num_hours=st.number_input(f"Number of {category} hours:")
-        st.write(" ")
-        st.write(" ")
     if not selected_categories:
         st.warning("Please select labour category to calculate service costs")
     else:
         data_list=[]
         grand_total=0
         for category in selected_categories:
-            num_people=st.session_state.get(category,0)
+            num_people_key=f"{category}_1"
+            num_hours_key=f"{category}_2"
+            col1, col2 = st.columns(2)
+            with col1:
+                num_people=st.number_input(f"Number of {category} personnel:",min_value=0,step=1,key=num_people_key) 
+            with col2:
+                num_hours=st.number_input(f"Number of {category} personnel:",min_value=0,step=1,key=num_hours_key)
             wage_type_df=df[wage_type]
             wage = df[(df['Country'] == selected_country) & (df['Job Title'] == category) ][wage_type].values[0]
             # latest_year=df[(df['Country'] == selected_country) & (df['Labour Type New'] == category)]['Year'].values[0]
             # st.write(monthly_wage)
-            annual_cost = num_people * wage 
+            annual_cost = num_people * wage * num_hours
             grand_total += annual_cost
             # st.write(f"{category}: {monthly_cost}")
-            data_list.append({"Labour Type":category,"Number of Units":num_people,"Wage":round(wage,0),"Number of hours":num_hours, "Cost":round(annual_cost,0)})
+            data_list.append({"Labour Type":category,"Number of Units":num_people," Hourly Wage":round(wage,2),"Total hours":num_hours, " Total Cost":round(annual_cost,2)})
         result_df=pd.DataFrame(data_list)
         st.write(" ")
         st.write(" ")
